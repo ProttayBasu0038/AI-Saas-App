9e3e3565a729e7f32e7b7dad85730051023811dd
ProttayBasu0038 <basuricky45@gmail.com>
2025-09-16 11:10:58 +0530

    project done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import NavBar from '../compnents/NavBar'
+import Hero from '../compnents/Hero'
+import AiTools from '../compnents/AiTools'
+import Testimonial from '../compnents/Testimonial'
+import Plan from '../compnents/Plan'
+import Footer from '../compnents/Footer'
+const Home = () => {
+  return (
+    <>
+       <NavBar/>
+       <Hero/>
+       <AiTools></AiTools>
+       <Testimonial/>
+       <Plan></Plan>
+       <Footer></Footer>
+    </>
+  )
+}
+
+export default Home
